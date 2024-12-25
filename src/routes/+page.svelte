526e9a1736b38051a4c49ef1aff9f8ca51733a2d
DoormatIka <trashcanartistlily@gmail.com>
2024-12-26 00:33:57 +0800

    redesign

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,13 @@
   let state: Orchestra;
   let scale = 0;
   let saveContent = "";
+  let errGlobal = "";
 
   onMount(async () => {
     state = new Orchestra();
     state.setStageClick();
-    await state.loadImage("https://images-ext-1.discordapp.net/external/j_0a_sMO9wIlnUS3S_2F98Xd57v6if9DoHqjZ32Jmk8/https/pbs.twimg.com/media/Ge6MO7qbAAAinSx.jpg%3Alarge?format=webp&width=670&height=670");
+    await state.loadImage("https://images-ext-1.discordapp.net/external/j_0a_sMO9wIlnUS3S_2F98Xd57v6if9DoHqjZ32Jmk8/https/pbs.twimg.com/media/Ge6MO7qbAAAinSx.jpg%3Alarge?format=webp&width=670&height=670")
+      .catch((err) => errGlobal = err);
     scale = state.getScale();
   })
   async function changeImage(e: SubmitEvent & {currentTarget: EventTarget & HTMLFormElement}) {
@@ -20,13 +22,16 @@
     const img_link = formProps["imagelink"];
     if (img_link) {
       await state.loadImage(img_link.toString())
+        .catch((err) => errGlobal = err);
     }
   }
   async function save() {
-    saveContent = await state.save();
+    saveContent = await state.save()
+      .catch((err) => errGlobal = err);
   }
   async function load() {
-    await state.load(saveContent);
+    await state.load(saveContent)
+      .catch((err) => errGlobal = err);
   }
   function undo() {
     state.undo();
@@ -44,43 +49,39 @@
   }
 </script>
 
-<div class="w-80 py-2">
-  <div>
-    <p>Input image</p>
-    <form class="flex gap-2" on:submit={changeImage}>
-      <input class="flex-1" type="text" name="imagelink" id="imagelink">
-      <button type="submit">Submit</button>
-    </form>
-  </div>
 
-  <br>
+<div class="flex flex-row items-center gap-3">
+  <button class="flex-1" on:click={undo}>Undo</button>
+  <button class="flex-1" on:click={redo}>Redo</button>
 
-  <div class="flex gap-2">
-    <button class="flex-1" on:click={undo}>Undo</button>
-    <button class="flex-1" on:click={redo}>Redo</button>
-    <button class="flex-1" on:click={() => { state.pushText(0, 0); }}>Add</button>
-  </div>
-
-  <br>
-
-  <div class="flex gap-2 flex-row h-6 items-center">
-    <button  on:click={zoomIn} class="flex-1 h-full">Zoom +</button>
-    <p style="padding: 0px 1em 0px 1em;">{scale}x</p>
-    <button on:click={zoomOut} class="flex-1 h-full">Zoom -</button>
-  </div>
-
-  <br>
-
-  <div class="flex flex-col items-center gap-2">
-    <div class="w-full">
-      <textarea class="w-full" name="save" id="save">{saveContent}</textarea>
-    </div>
-    <div class="flex w-full gap-2">
-      <button on:click={save} class="flex-1 h-full">Save</button>
-      <button on:click={load} class="flex-1 h-full">Load</button>
-    </div>
-  </div>
+  <button on:click={zoomIn} class="flex-1 h-full">Zoom +</button>
+  <p style="padding: 0px 1em 0px 1em;">{scale}x</p>
+  <button on:click={zoomOut} class="flex-1 h-full">Zoom -</button>
 </div>
 
+<br>
 
-<div id="container" class="border-dotted w-fit"></div>
+<p>Double-click to add text.</p>
+<div id="container" class="border-dotted w-fit bg-transparent"></div>
+
+<br>
+
+<div>
+  <p>Input image</p>
+  <form class="flex gap-2" on:submit={changeImage}>
+    <input class="flex-1" type="text" name="imagelink" id="imagelink">
+    <button type="submit">Submit</button>
+  </form>
+</div>
+
+<br>
+
+<div class="flex flex-col items-center gap-2">
+  <div class="w-full">
+    <textarea class="w-full" name="save" id="save">{saveContent}</textarea>
+  </div>
+  <div class="flex w-full gap-2">
+    <button on:click={save} class="flex-1 h-full">Save</button>
+    <button on:click={load} class="flex-1 h-full">Load</button>
+  </div>
+</div>
